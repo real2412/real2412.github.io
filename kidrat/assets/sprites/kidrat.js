97690e9a23b56ee11ff6b60d78fc6724a0c03869
Renan Salazar <ramsp2412@gmail.com>
2017-09-25 10:20:17 -0500

    Commit message

---
+++ kidrat/assets/sprites/kidrat.js
@@ -0,0 +1,30 @@
+{
+"frames": {
+	"0.png": {
+		"frame": {"x":0, "y":31, "w":32, "h":30},
+		"spriteSourceSize": {"x":0,"y":2,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	},
+	"1.png": {
+		"frame": {"x":33, "y":0, "w":32, "h":30},
+		"spriteSourceSize": {"x":0,"y":0,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	},
+	"2.png": {
+		"frame": {"x":33, "y":31, "w":32, "h":30},
+		"spriteSourceSize": {"x":0,"y":2,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	},
+	"3.png": {
+		"frame": {"x":0, "y":0, "w":32, "h":30},
+		"spriteSourceSize": {"x":0,"y":0,"w":32,"h":32},
+		"sourceSize": {"w":32,"h":32}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 66, "h": 62},
+	"scale": "1"
+}
+}
